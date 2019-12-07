5cc8303b6bcc671e4edbda3534ab4884704b8648
Stephano <stephano.wurttele@utec.edu.pe>
2019-12-07 02:04:33 -0500

    find

--- btree.h
+++ btree.h
@@ -8,30 +8,6 @@ class BTree {
     private:
         Node<T>* root;
         unsigned int degree;
-        Node<T> find(T data){ 
-            if(!root)
-                return false;
-            Node<T>* temp=root;
-            vector<T> tempvec=temp->keys;
-            while(temp!=nullptr){
-                for (int i =0 ; i < temp->size ; i++){
-                    if(tempvec[i]==data)
-                        return temp;
-                    else{
-                        if(data>tempvec[i]{
-                            if(i==degree-1)
-                                temp=temp->childs[i+1];
-                                break;
-                        }
-                        else{
-                            temp=temp->childs[i];
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
     public:
         BTree(unsigned int degree) : degree(degree), root(nullptr) {};
 
@@ -39,26 +15,19 @@ class BTree {
             if(!root)
                 return false;
             Node<T>* temp=root;
-            vector<T> tempvec=temp->keys;
-            while(temp!=nullptr){
-                for (int i =0 ; i < temp->size ; i++){
-                    if(tempvec[i]==data)
+            while(true){
+                for (int i =0 ; i < temp->keys.size() ; i++){
+                    if(temp->keys[i]==data)
                         return true;
-                    else{
-                        if(data>tempvec[i]{
-                            if(i==degree-1)
-                                temp=temp->childs[i+1];
-                                break;
-                            
-                        }
-                        else{
-                            temp=temp->childs[i];
+                    else
+                        if(data<temp->keys[i]
                             break;
-                        }
-                    }
                 }
-                
-            } 
+                if(!temp->isLeaf)
+                    temp=temp->childs[i];
+                else break;
+            }
+        }
 
         bool insert(T data) {
             
